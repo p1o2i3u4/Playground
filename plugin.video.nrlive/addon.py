16f96c177570906768a0eed8edd8139fbf796359
p1o2i3u4 <syshin.uc@gmail.com>
2016-01-16 00:22:26 -0700

    nrl

--- plugin.video.nrlive/addon.py
+++ plugin.video.nrlive/addon.py
@@ -15,11 +15,29 @@ _connectionTimeout = 20
 _header = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3'
 tablet_UA = "Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Safari/535.19"
 
+##채널 이름##
+ad=['','','KBS N Sorts','SBS Sports','MBC Sports+','','','','','','','','','','','','','','','','']
 
 def listMainCategories():
     addDir("초고화질", " ", "High", '')  
     addDir("고화질", " ", "Med", '')
     addDir("저화질", " ", "Low", '')
+    addDir("티비", " ", "tv", '')
+
+
+def tv():
+    addLink('SBS', 'rtmp://rtmpplay3.idol001.com/live/korea_sbs', 'livetv', '')
+    addLink('MBC', 'rtmp://rtmpplay2.idol001.com/live/korea_mbc', 'livetv', '')
+    addLink('KBS1', 'rtmp://rtmpplay3.idol001.com/live/korea_kbs1', 'livetv', '')
+    addLink('KBS2', 'rtmp://rtmpplay3.idol001.com/live/korea_kbs2', 'livetv', '')
+    addLink('JTBC', 'rtmp://rtmpplay3.idol001.com/live/korea_jtbc', 'livetv', '')
+    addLink('ONSTYLE', 'rtmp://rtmpplay3.idol001.com/live/korea_onstyle', 'livetv', '')
+    addLink('MBC MUSIC', 'rtmp://rtmpplay3.idol001.com/live/korea_mbcmusic', 'livetv', '')
+    addLink('Mnet', 'rtmp://rtmpplay3.idol001.com/live/korea_mnet', 'livetv', '')
+    addLink('TVN', 'rtmp://rtmpplay3.idol001.com/live/korea_tvn', 'livetv', '')
+    addLink('arirang', 'rtmp://rtmpplay3.idol001.com/live/korea_arirang', 'livetv', '')
+    addLink('SBS MTV', 'rtmp://rtmpplay3.idol001.com/live/korea_sbsmtv', 'livetv', '')
+    addLink('MBC everyone', 'rtmp://rtmpplay3.idol001.com/live/korea_mbcevery1', 'livetv', '')
     
 def High_list(url):
     try:
@@ -43,19 +61,36 @@ def High_list(url):
                 addLink(match3[i][1], match3[i][2], 'resolveAndPlayVideo', '')
             else:
                 continue
+            
+        addDir('네이버 생방송 목록',' ','High_Live', '')
+        addLink('##이하 다음 생중계##',' ','', '')
+        
+        ## daum ##
+        url='http://live.tvpot.daum.net/potplayer/service/LiveTimeTable.do'
+        req = urllib2.Request(url)
+        req.add_header('Accept-Langauge', 'ko')
+        req.add_header('Cookie', 'language=kr')
+        link = urllib2.urlopen(req).read()
+        soup=BeautifulSoup(link)
+        
+        items = []
+        for node in soup.findAll('li', {'class':'on'}):
+            if not node.span:
+                continue
+            time=node.span.string
+            title = node.find('a', {'class': 'link_live cast_title'}).string.replace('&amp;','&').replace('&lt;','<').replace('&gt;','>').replace('&#039;','\'')
+            liveid = node.a['href']
+            liveid=int(re.search(r'\d+', liveid).group())
+            url='http://videofarm.daum.net/controller/api/open/v1_0/BroadcastStreams.action?broadcastId='+str(liveid)+'&profile=HIGH'
+            items.append({'title':title, 'broad_date':time, 'url':url, 'thumbnail':''})
+            items.sort(reverse=True)
 
-        addLink('SBS', 'rtmp://rtmpplay3.idol001.com/live/korea_sbs', 'livetv', '')
-        addLink('MBC', 'rtmp://rtmpplay3.idol001.com/live/korea_mbc', 'livetv', '')
-        addLink('KBS1', 'rtmp://rtmpplay3.idol001.com/live/korea_kbs1', 'livetv', '')
-        addLink('KBS2', 'rtmp://rtmpplay3.idol001.com/live/korea_kbs2', 'livetv', '')
-        addLink('JTBC', 'rtmp://rtmpplay3.idol001.com/live/korea_jtbc', 'livetv', '')
-        addLink('ONSTYLE', 'rtmp://rtmpplay3.idol001.com/live/korea_onstyle', 'livetv', '')
-        addLink('MBC MUSIC', 'rtmp://rtmpplay3.idol001.com/live/korea_mbcmusic', 'livetv', '')
-        addLink('Mnet', 'rtmp://rtmpplay3.idol001.com/live/korea_mnet', 'livetv', '')
-        addLink('TVN', 'rtmp://rtmpplay3.idol001.com/live/korea_tvn', 'livetv', '')
-        addLink('arirang', 'rtmp://rtmpplay3.idol001.com/live/korea_arirang', 'livetv', '')
-        addLink('SBS MTV', 'rtmp://rtmpplay3.idol001.com/live/korea_sbsmtv', 'livetv', '')
-        addLink('MBC everyone', 'rtmp://rtmpplay3.idol001.com/live/korea_mbcevery1', 'livetv', '')
+        for i in range(len(items)):
+            items[i] = (items[i]['title'], items[i]['url'])
+              
+
+        for name, url in items:
+            addLink(name, url, 'resolveAndPlayVideoDaum', '')
 
     except urllib2.URLError:
         addLink("성용이를 불러주세용.", '', '', '')
@@ -83,6 +118,38 @@ def Med_list(url):
             else:
                 continue
             
+        addDir('네이버 생방송 목록',' ','Med_Live', '')
+        addLink('##이하 다음 생중계##',' ','', '')
+        
+        ## daum ##
+        url='http://live.tvpot.daum.net/potplayer/service/LiveTimeTable.do'
+        req = urllib2.Request(url)
+        req.add_header('Accept-Langauge', 'ko')
+        req.add_header('Cookie', 'language=kr')
+        link = urllib2.urlopen(req).read()
+        soup=BeautifulSoup(link)
+        
+        items = []
+        for node in soup.findAll('li', {'class':'on'}):
+            if not node.span:
+                continue
+            time=node.span.string
+            title = node.find('a', {'class': 'link_live cast_title'}).string.replace('&amp;','&').replace('&lt;','<').replace('&gt;','>').replace('&#039;','\'')
+            liveid = node.a['href']
+            liveid=int(re.search(r'\d+', liveid).group())
+            url='http://videofarm.daum.net/controller/api/open/v1_0/BroadcastStreams.action?broadcastId='+str(liveid)+'&profile=MAIN'
+            items.append({'title':title, 'broad_date':time, 'url':url, 'thumbnail':''})
+            items.sort(reverse=True)
+
+        for i in range(len(items)):
+            items[i] = (items[i]['title'], items[i]['url'])
+              
+
+        for name, url in items:
+            addLink(name, url, 'resolveAndPlayVideoDaum', '')
+
+        
+            
     except urllib2.URLError:
         addLink("성용이를 불러주세용.", '', '', '')
 
@@ -108,11 +175,57 @@ def Low_list(url):
                 addLink(match3[i][1], match3[i][2], 'resolveAndPlayVideo_low', '')
             else:
                 continue
-            
+
+        addDir('네이버 생방송 목록',' ','Low_Live', '')
+        addLink('##이하 다음 생중계##',' ','', '')
+        
+        ## daum ##
+        url='http://live.tvpot.daum.net/potplayer/service/LiveTimeTable.do'
+        req = urllib2.Request(url)
+        req.add_header('Accept-Langauge', 'ko')
+        req.add_header('Cookie', 'language=kr')
+        link = urllib2.urlopen(req).read()
+        soup=BeautifulSoup(link)
+        
+        items = []
+        for node in soup.findAll('li', {'class':'on'}):
+            if not node.span:
+                continue
+            time=node.span.string
+            title = node.find('a', {'class': 'link_live cast_title'}).string.replace('&amp;','&').replace('&lt;','<').replace('&gt;','>').replace('&#039;','\'')
+            liveid = node.a['href']
+            liveid=int(re.search(r'\d+', liveid).group())
+            url='http://videofarm.daum.net/controller/api/open/v1_0/BroadcastStreams.action?broadcastId='+str(liveid)+'&profile=BASE'
+            items.append({'title':title, 'broad_date':time, 'url':url, 'thumbnail':''})
+            items.sort(reverse=True)
+
+        for i in range(len(items)):
+            items[i] = (items[i]['title'], items[i]['url'])
+              
+
+        for name, url in items:
+            addLink(name, url, 'resolveAndPlayVideoDaum', '')
+
+        
     except urllib2.URLError:
         addLink("성용이를 불러주세용.", '', '', '')
 
-
+def resolveAndPlayVideoDaum(url):
+    try:
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        
+        #match=re.compile('"rtsp":"(.*?)"').search(link).group(1)
+        match=re.compile('"rtmp":"(.*?)"').search(link).group(1)
+        listItem = xbmcgui.ListItem(path=str(match))
+        listItem.setProperty('IsPlayable', 'true')
+        xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+        
+    except urllib2.URLError:
+        addLink("성용이를 불러주세용.", '', '', '')
+        
 def resolveAndPlayVideo(url):
     try:
         url2='http://sports.news.naver.com/tv/index.nhn?gameId=' + url
@@ -186,6 +299,21 @@ def resolveAndPlayVideo_low(url):
     except urllib2.URLError:
         addLink("성용이를 불러주세용.", '', '', '')
 
+def resolveAndPlayVideoLive(url):
+    try:
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        
+        listItem = xbmcgui.ListItem(path=str(link))
+        listItem.setProperty('IsPlayable', 'true')
+        xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+        
+    except urllib2.URLError:
+        addLink("성용이를 불러주세용.", '', '', '')
+    
+
 def livetv(url):
     try:
         listItem = xbmcgui.ListItem(path=str(url))
@@ -194,7 +322,192 @@ def livetv(url):
         
     except urllib2.URLError:
         addLink("성용이를 불러주세용.", '', '', '')
-    
+
+def High_Live_List(url):
+    try:
+        f=range(1,15)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+            if match == 'on':
+                name='채널 '+str(i)
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_2000.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+                
+        f=range(100,102)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+            if match == 'on':
+                name='채널 '+str(i)
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_2000.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+
+        f=range(200,212)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+            
+            if match == 'on':
+                name='채널 '+str(i)
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_2000.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+                
+        f=range(1,15)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highad'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+                       
+            if match == 'on':
+                
+                name='ad채널 '+str(i) +' '+ad[i]
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fad'+str(i)+'%2F_definst_%2Fad'+str(i)+'_2000.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+      
+    except urllib2.URLError:
+        addLink("성용이를 불러주세용.", '', '', '')
+
+def Med_Live_List(url):
+    try:
+        f=range(1,15)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            if match == 'on':
+                name='채널 '+str(i)
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_800.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+                
+        f=range(100,102)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            if match == 'on':
+                name='채널 '+str(i)
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_800.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+
+        f=range(200,212)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            if match == 'on':
+                name='채널 '+str(i)
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_800.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+                
+        f=range(1,15)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highad'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            
+            if match == 'on':
+                name='ad채널 '+str(i) +' '+ad[i]
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fad'+str(i)+'%2F_definst_%2Fad'+str(i)+'_800.stream%2Fplaylist.m3u8'
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+      
+    except urllib2.URLError:
+        addLink("성용이를 불러주세용.", '', '', '')
+
+def Low_Live_List(url):
+    try:
+        f=range(1,15)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            if match == 'on':
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_300.stream%2Fplaylist.m3u8'
+                name='채널 '+str(i)
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+                
+        f=range(100,102)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            if match == 'on':
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_300.stream%2Fplaylist.m3u8'
+                name='채널 '+str(i)
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+
+        f=range(200,212)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highch'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+          
+            if match == 'on':
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fch'+str(i)+'%2F_definst_%2Fch'+str(i)+'_300.stream%2Fplaylist.m3u8'
+                name='채널 '+str(i)
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+                
+        f=range(1,15)
+        for i in f:
+            url='http://cvapi.ncast.nhncorp.com/chStatus.nhn?chid=highad'+str(i)
+            req = urllib2.Request(url)
+            response = urllib2.urlopen(req)
+            link=response.read()
+            response.close()
+            match=re.compile('"status":"(.*?)"').search(link).group(1)
+
+            
+            if match == 'on':
+                link='http://surl.ncast.nhncorp.com/secUrl.nhn?orgUrl=http%3A%2F%2Fhls.live.m.nhn.gscdn.com%2Fad'+str(i)+'%2F_definst_%2Fad'+str(i)+'_300.stream%2Fplaylist.m3u8'
+                name='ad채널 '+str(i)  +' '+ad[i]
+                addLink(name, link, 'resolveAndPlayVideoLive', '')
+      
+    except urllib2.URLError:
+        addLink("성용이를 불러주세용.", '', '', '')
+
 def addLink(name,url,mode,iconimage):
     u=_pluginName+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
     ok=True
@@ -253,6 +566,8 @@ else:
         Med_list(urlUnquoted)
     elif params["mode"] == 'Low':
         Low_list(urlUnquoted)
+    elif params["mode"] == 'tv':
+        tv(urlUnquoted)
 
     elif params["mode"] == 'High_Live':
         High_Live_List(urlUnquoted)
@@ -267,6 +582,10 @@ else:
         resolveAndPlayVideo_med(urlUnquoted)
     elif params["mode"] == 'resolveAndPlayVideo_low':
         resolveAndPlayVideo_low(urlUnquoted)
+    elif params["mode"] == 'resolveAndPlayVideoDaum':
+        resolveAndPlayVideoDaum(urlUnquoted)
+    elif params["mode"] == 'resolveAndPlayVideoLive':
+        resolveAndPlayVideoLive(urlUnquoted)
     elif params["mode"] == 'livetv':
         livetv(urlUnquoted)        
 xbmcplugin.endOfDirectory(_thisPlugin)        

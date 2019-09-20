ca2da018417aa656c4a81e80a275b5fb627d062c
ssy080 <powerw6@gmail.com>
2019-09-19 20:50:42 -0600

    test

--- plugin.video.ODC3/addon.py
+++ plugin.video.ODC3/addon.py
@@ -9,6 +9,7 @@ import requests
 import xbmcplugin, xbmcgui, sys
 import urllib2, urllib, re
 from BeautifulSoup import BeautifulSoup
+from datetime import date
 
 plugin = Plugin()
 
@@ -98,7 +99,8 @@ def listRecentCategories(url):
     print(r.status_code, r.reason)
     obj = json.loads(r.text)
 
-
+    print "testingx"
+    print obj
     result = []
     for item in obj['data'][2]['data']:
         title=item['title']+' - '+item['broadcastDate']
@@ -111,7 +113,7 @@ def listRecentCategories(url):
             thumb='https://sp.ondemandkorea.com'+thumb
         
         #if item['plusOnly']=='0':
-        eid=item['id']+'^'+item['plusOnly']+'^'+item['slug']
+        eid=item['id']+'^'+item['plusOnly']+'^'+item['slug']+'^'+item['categoryId']+'^'+item['broadcastDate']
         result.append([title, eid, thumb])
 
     result.append(['##예능##', '', ''])
@@ -126,11 +128,11 @@ def listRecentCategories(url):
         else:
             thumb='https://sp.ondemandkorea.com'+thumb
         #if item['plusOnly']=='0':   
-        eid=item['id']+'^'+item['plusOnly']+'^'+item['slug']
+        eid=item['id']+'^'+item['plusOnly']+'^'+item['slug']+'^'+item['categoryId']+'^'+item['broadcastDate']
         result.append([title, eid, thumb])
                               
     for name, url2, thumbnail in result:
-        addLink(name, url2, 'resolveAndPlayVideo', thumbnail)
+        addLink(name, url2, 'resolveAndPlayVideoRecent', thumbnail)
     
 def listVideoCategories(url):
     url2='https://api.ondemandkorea.com/api3/category/'
@@ -247,7 +249,9 @@ def listdramaInCategory(url):
     print(r.status_code, r.reason)
     obj = json.loads(r.text)
 
-
+    #print obj
+    src=obj['program']['topEpisode']['videoSrc']
+    
     result = []
     for item in obj['data']:
         title=item['title']+' - '+item['broadcastDate']
@@ -258,7 +262,15 @@ def listdramaInCategory(url):
             thumb=thumb
         else:
             thumb='https://sp.ondemandkorea.com'+thumb
-        eid=item['id']+'^'+item['plusOnly']+'^'+item['slug']
+
+                            
+        inds= [i for i,c in enumerate(src) if c=='/']
+        src2= src.replace(src[inds[-2]+1:inds[-1]], item['slug'])
+
+        inds= [i for i,c in enumerate(src2) if c=='/']
+        src3=src2.replace(src2[inds[4]+1:inds[5]], item['broadcastDate'][-4]+item['broadcastDate'][-3]+item['broadcastDate'][-2]+item['broadcastDate'][-1]+item['broadcastDate'][0]+item['broadcastDate'][1])
+            
+        eid=item['id']+'^'+item['plusOnly']+'^'+item['slug']+'^'+src3
         result.append([title, eid,thumb ])
 
     for name, url2, thumbnail in result:
@@ -267,6 +279,8 @@ def listdramaInCategory(url):
 def resolveAndPlayVideo(url):
     url=url.split('%5E')
     plus=0
+    today = date.today()
+
     print url
     if plus==0:
         url2='https://api.ondemandkorea.com/api3/episode/'
@@ -286,9 +300,9 @@ def resolveAndPlayVideo(url):
         r = requests.post(url2, data=data, headers=headers)
         obj = json.loads(r.text)
 
-
         use_mp4_url = plugin.get_setting('mp4_url', bool)
         quality = plugin.get_setting("quality", str)
+        prefix = plugin.get_setting("prefix", str)
         if use_mp4_url:
             if quality == '1':
                 listItem = xbmcgui.ListItem(path=obj['episode']['mp4Src'][1]['src'])
@@ -313,14 +327,123 @@ def resolveAndPlayVideo(url):
                     listItem = xbmcgui.ListItem(path=url)
                     xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
         else:
-##            if quality == '1':
-##                vurl1=obj['episode']['videoSrc']
-##                vurl=vurl1.replace('1080','720')
-##                plugin.play_video( {'label': title, 'path':vurl} )
-##            else:
-            listItem = xbmcgui.ListItem(path=obj['episode']['videoSrc'])
-            xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
-    
+
+      
+            try:
+                #print "testingx"
+                if quality == '1':
+                    url[3]=url[3].replace('%3A',':').replace('%2F','/')
+
+                    
+                    print url[3]
+                    listItem = xbmcgui.ListItem(path=url[3])
+                    xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+                    
+                else:
+                    url[3]=url[3].replace('%3A',':').replace('%2F','/')
+                    url[3]=url[3].replace('720','1080')
+                    
+                    print url[3]
+                    listItem = xbmcgui.ListItem(path=url[3])
+                    xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+            except:
+                print obj['episode']['videoSrc']
+                listItem = xbmcgui.ListItem(path=obj['episode']['videoSrc'])
+                xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+
+
+def resolveAndPlayVideoRecent(url):
+    url=url.split('%5E')
+    plus=0
+    today = date.today()
+
+    print url
+    if plus==0:
+        url2='https://api.ondemandkorea.com/api3/episode/'
+        data={'language': '0',
+                       'adid': 'a36f8a5d-edae-44b7-b88d-ff0981008535',
+                       'appShorterVersion':'1.8.93',
+                       'idfv':'ef4212b0a9778005',
+                       'screenid':'0',
+                       'appBundleVersion':'1.8.93',
+                       'accessToken': token,
+                       'id':url[0]
+              }
+        headers={
+            'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+            'User-Agent': 'MMozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Safari/535.19',
+                }
+        r = requests.post(url2, data=data, headers=headers)
+        obj = json.loads(r.text)
+
+        use_mp4_url = plugin.get_setting('mp4_url', bool)
+        quality = plugin.get_setting("quality", str)
+        prefix = plugin.get_setting("prefix", str)
+
+        try:
+            name=re.findall(".*?-e[0-9]",url[2])[0]
+        except:
+            name2=re.findall(".*?-[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]",url[2])[0]
+
+        try:
+            name
+        except NameError:
+            if url[3]=="2":
+                base_url = "http://odk-hls.akamaized.net/"+prefix+"/v1/"+url[4][-4]+url[4][-3]+url[4][-2]+url[4][-1]+url[4][0]+url[4][1]+"/drama/"+ name2[:-9] +"/"+url[2]+"/playlist_720.m3u8"
+            else:
+                base_url = "http://odk-hls.akamaized.net/"+prefix+"/v1/"+url[4][-4]+url[4][-3]+url[4][-2]+url[4][-1]+url[4][0]+url[4][1]+"/variety/"+ name2[:-9] +"/"+url[2]+"/playlist_720.m3u8"
+        else:
+            if url[3]=="2":
+                base_url = "http://odk-hls.akamaized.net/"+prefix+"/v1/"+url[4][-4]+url[4][-3]+url[4][-2]+url[4][-1]+url[4][0]+url[4][1]+"/drama/"+ name[:-3] +"/"+url[2]+"/playlist_720.m3u8"
+            else:
+                base_url = "http://odk-hls.akamaized.net/"+prefix+"/v1/"+url[4][-4]+url[4][-3]+url[4][-2]+url[4][-1]+url[4][0]+url[4][1]+"/variety/"+ name[:-3] +"/"+url[2]+"/playlist_720.m3u8"
+        
+        if use_mp4_url:
+            if quality == '1':
+                listItem = xbmcgui.ListItem(path=obj['episode']['mp4Src'][1]['src'])
+                xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+            
+            else:
+                try:
+                    f = urllib2.Request(obj['episode']['mp4Src'][0]['src'])
+                    f.add_header('User-Agent', default_hdr)
+                    f=urllib2.urlopen(f)    
+                    dead = False
+                except:
+                    dead = True
+           
+                if dead==False:
+                    listItem = xbmcgui.ListItem(path=obj['episode']['mp4Src'][0]['src'])
+                    xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+                else:
+                    add=re.compile('/v1/[0-9]+/').search(obj['episode']['videoSrc']).group()
+                    url=obj['episode']['mp4Src'][0]['src']
+                    url=url.replace('ondemandkorea.com/','ondemandkorea.com/'+add)
+                    listItem = xbmcgui.ListItem(path=url)
+                    xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+        else:
+
+      
+            try:
+                #print "testingx"
+                if quality == '1':
+                   
+                    #print base_url
+                    listItem = xbmcgui.ListItem(path=base_url)
+                    xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+                    
+                else:
+                    base_url=base_url.replace('720','1080')
+                    
+                    print base_url
+                    listItem = xbmcgui.ListItem(path=base_url)
+                    xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+            except:
+                print obj['episode']['videoSrc']
+                listItem = xbmcgui.ListItem(path=obj['episode']['videoSrc'])
+                xbmcplugin.setResolvedUrl(_thisPlugin, True, listItem)
+
+                
 def addLink(name,url,mode,iconimage):
     u=_pluginName+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
     liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
@@ -379,6 +502,8 @@ else:
         
     elif params["mode"] == 'resolveAndPlayVideo':
         resolveAndPlayVideo(url)
+    elif params["mode"] == 'resolveAndPlayVideoRecent':
+        resolveAndPlayVideoRecent(url)
 ##    elif params["mode"] == 'resolveAndPlayMovie':
 ##        resolveAndPlayMovie(urlUnquoted)
 ##
